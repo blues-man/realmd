f0986df60574b364054d74971c2660d6117d6213
Natale Vinto <ebballon@gmail.com>
2017-06-09 18:52:38 +0200

    Add support for password to kinit

--- manifests/join/keytab.pp
+++ manifests/join/keytab.pp
@@ -5,20 +5,15 @@
 #
 class realmd::join::keytab {
 
-  $_domain            = $::realmd::domain
-  $_domain_join_user  = $::realmd::domain_join_user
-  $_krb_keytab        = $::realmd::krb_keytab
-  $_krb_config_file   = $::realmd::krb_config_file
-  $_krb_config        = $::realmd::krb_config
-  $_manage_krb_config = $::realmd::manage_krb_config
+  $_domain                = upcase($::realmd::domain)
+  $_domain_join_user      = $::realmd::domain_join_user
+  $_domain_join_password  = $::realmd::domain_join_password
+  $_krb_keytab            = $::realmd::krb_keytab
+  $_krb_config_file       = $::realmd::krb_config_file
+  $_krb_config            = $::realmd::krb_config
+  $_manage_krb_config     = $::realmd::manage_krb_config
 
-  file { 'krb_keytab':
-    path   => $_krb_keytab,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0400',
-    notify => Exec['run_kinit_with_keytab'],
-  }
+
 
   if $_manage_krb_config {
     file { 'krb_configuration':
@@ -33,6 +28,15 @@ class realmd::join::keytab {
   }
 
   if $_krb_keytab != undef {
+
+    file { 'krb_keytab':
+      path   => $_krb_keytab,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0400',
+      notify => Exec['run_kinit_with_keytab'],
+    }
+
     exec { 'run_kinit_with_keytab':
       path        => '/usr/bin:/usr/sbin:/bin',
       command     => "kinit -kt ${_krb_keytab} ${_domain_join_user}",
@@ -42,7 +46,7 @@ class realmd::join::keytab {
   } else {
     exec { 'run_kinit_with_keytab':
       path        => '/usr/bin:/usr/sbin:/bin',
-      command     => "kinit ${_domain_join_user}@${_domain}",
+      command     => "echo '${_domain_join_password}' | kinit ${_domain_join_user}@${_domain}",
       refreshonly => true,
       before      => Exec['realm_join_with_keytab'],
     }
@@ -52,7 +56,7 @@ class realmd::join::keytab {
   exec { 'realm_join_with_keytab':
     path    => '/usr/bin:/usr/sbin:/bin',
     command => "realm join ${_domain}",
-    unless  => "klist -k /etc/krb5.keytab | grep -i '${::hostname[0,15]}@${_domain}'",
+    unless  => "klist -k /etc/krb5.keytab | grep -i $(hostname -s)@${_domain}'",
     require => Exec['run_kinit_with_keytab'],
   }
 
