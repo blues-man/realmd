a39c3744ab16c9c168a7cb01cd75b2ca8d75d9d8
Natale Vinto <ebballon@gmail.com>
2017-06-23 11:57:12 +0200

    Fix hostname check in klist

--- manifests/join/keytab.pp
+++ manifests/join/keytab.pp
@@ -48,7 +48,7 @@ class realmd::join::keytab {
     exec { 'run_kinit_with_keytab':
       path        => '/usr/bin:/usr/sbin:/bin',
       command     => "echo '${_domain_join_password}' | kinit ${_domain_join_user}@${_domain}",
-      unless  => "klist -k /etc/krb5.keytab | grep -i $(hostname -s)@${_domain}",
+      unless  => "klist -k /etc/krb5.keytab | grep -i $(hostname)@${_domain}",
       before      => Exec['realm_join_with_keytab'],
     }
 
@@ -57,7 +57,7 @@ class realmd::join::keytab {
   exec { 'realm_join_with_keytab':
     path    => '/usr/bin:/usr/sbin:/bin',
     command => "realm join ${_domain}",
-    unless  => "klist -k /etc/krb5.keytab | grep -i $(hostname -s)@${_domain}",
+    unless  => "klist -k /etc/krb5.keytab | grep -i $(hostname)@${_domain}",
     require => Exec['run_kinit_with_keytab'],
   }
 
